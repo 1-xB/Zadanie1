9e531783d633924726eb95548b272d41ee887be5
Piotr Zarzycki <piotr.zarzyckidev@proton.me>
2025-09-29 12:35:19 +0200

    Ukonczone zadanie

--- Program.cs
+++ Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string readTxt = File.ReadAllText("C:/test/test_piotr_zarzycki.txt");
+        Console.WriteLine("Tekst z pliku txt : " + readTxt);
+        Console.WriteLine("Ilość 'a' w tym pliku : " + readTxt.Count(s => s == 'a'));
+        
+        // lub 2 sposob.
+        
+        int a = 0;
+        for (int i = 0; i < readTxt.Length; i++)
+        {
+            if (readTxt[i] == 'a')
+            {
+                a += 1;
+            }
+        }
+        
     }
 }
